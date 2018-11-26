5d64e26c4e2e0da0a0aabe5dcb5b1f3ef9609b5d
Edward <edwardlshfzw@gmail.com>
2018-11-26 22:11:13 +0800

    删除不需要追踪的东西

---
+++ test.py
@@ -0,0 +1,24 @@
+# goods = [{'name': '大宝SOD蜜', 'price': 10}, {'name': '洗面奶', 'price': 50}, {'name': '老面包', 'price': 15},
+#          {'name': '小米电视', 'price': 4000},
+#          {'name': '冰箱', 'price': 3000}, {'name': '海尔洗衣机', 'price': 4000},
+#          {'name': 'macbook air', 'price': 10000}, {'name': 'thinkpad', 'price': 8000},
+#          {'name': 'iphoneXS', 'price': 12000}, {'name': '老男孩教育', 'price': 13000}]
+#
+# for index, line in enumerate(goods):
+#     print(index, line['name'], line['price'])
+#
+# print(len(goods))
+
+# goods = dict([{'name': '洗面奶', 'price': 50}])
+#
+# for i in goods:
+#     print(i)
+
+# a = [{'name': '大宝SOD蜜', 'price': 10}]
+# dic = {}
+# for i in a:
+#     dic['name'] = i['name']
+#     dic['price'] = i['price']
+# print(dic)
+
+
